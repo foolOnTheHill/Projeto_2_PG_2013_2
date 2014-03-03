f402f799616e3a06e4fe00e751795b5429390aac
George H. Alvares <blackbird.fly95@gmail.com>
2014-03-03 21:43:00 -0200

    Create Inputs.java
    Classe que irá tratar da leitura das entradas.

---
+++ src/Inputs.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+
+
+public class Inputs {
+	
+	public static Camera getCamera(BufferedReader camera) {
+		return null;
+	}
+	
+	public static Iluminacao getIluminacao(BufferedReader iluminacao) {
+		return null;
+	}
+	
+	public static Objeto getObjeto(BufferedReader objeto) {
+		return null;
+	}
+	
+}
