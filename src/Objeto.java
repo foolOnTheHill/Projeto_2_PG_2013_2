151ae6a309958b632bb3ef9f0ffecad27908ae0a
George H. Alvares <blackbird.fly95@gmail.com>
2014-03-03 21:02:39 -0200

    Create Objeto.java
    Classe com as informações do objeto que será renderizado.

---
+++ src/Objeto.java
@@ -0,0 +1,22 @@
+
+public class Objeto {
+	
+	Ponto[] visao; // Pontos do objeto em coordenadas de vista
+	Ponto[] screen; // Pontos do objeto em coordenadas de tela (i.e., 2D)
+
+	double[][] triangulos;
+	
+	/*Pré-processamento*/
+	Ponto[] normais_triangulos;
+	Ponto[] normais_vertices;
+	//
+
+	public Objeto(Ponto[] visao, Ponto[] screen, Ponto[] normais_triangulos, Ponto[] normais_vertices, double[][] triangulos) {
+		this.visao = visao;
+		this.screen = screen;
+		this.normais_triangulos = normais_triangulos;
+		this.normais_vertices = normais_vertices;
+		this.triangulos = triangulos;
+	}
+	
+}
